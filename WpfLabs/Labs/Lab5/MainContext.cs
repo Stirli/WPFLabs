b86abe9257b1299fb2f6486e2816a3dbb7291512
Unknown <d.v.stanski@outlook.com>
2018-01-28 22:15:44 +0300

    Кажется сделано

---
+++ WpfLabs/Labs/Lab5/MainContext.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Lab5.Annotations;
+
+namespace Lab5
+{
+    public class MainContext:INotifyPropertyChanged
+    {
+        private ShapeData _shapeData;
+        private ObservableCollection<ShapeData> _shapes;
+        private FileInfo _fileInfo;
+
+        public ShapeData ShapeData
+        {
+            get { return _shapeData; }
+            set
+            {
+                if (Equals(value, _shapeData)) return;
+                _shapeData = value;
+                OnPropertyChanged("ShapeData");
+            }
+        }
+
+        public ObservableCollection<ShapeData> Shapes
+        {
+            get { return _shapes; }
+            set
+            {
+                if (Equals(value, _shapes)) return;
+                _shapes = value;
+                OnPropertyChanged("Shapes");
+            }
+        }
+
+        public FileInfo FileInfo
+        {
+            get { return _fileInfo; }
+            set
+            {
+                if (Equals(value, _fileInfo)) return;
+                _fileInfo = value;
+                OnPropertyChanged("FileInfo");
+            }
+        }
+
+
+        public MainContext()
+        {
+            ShapeData = new ShapeData();
+            Shapes = new ObservableCollection<ShapeData>();
+            FileInfo = new FileInfo("Безымянный.vif");
+        }
+
+
+        public void AddShape()
+        {
+            Shapes.Add(ShapeData.Clone());
+        }
+
+
+
+        public void Load(string path)
+        {
+            Shapes.Clear();
+            IEnumerable<string> lines = File.ReadLines(path);
+            foreach (string line in lines)
+            {
+                ShapeData shapeData = (ShapeData)line;
+                Shapes.Add(shapeData);
+            }
+            FileInfo = new FileInfo(path);
+        }
+        public void Save(string path)
+        {
+            File.WriteAllLines(path, Shapes.ToList().ConvertAll(input => input.ToString()));
+            FileInfo = new FileInfo(path);
+        }
+
+        public bool CanSave
+        {
+            get
+            {
+                return Shapes.Count > 0;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- WpfLabs/Labs/Lab5/MainWindow.xaml.cs
+++ WpfLabs/Labs/Lab5/MainWindow.xaml.cs
@@ -19,73 +19,20 @@ namespace Lab5
     /// </summary>
     public partial class MainWindow : Window
     {
-
-        private ShapeData _shape;
-        private List<ShapeData> _shapes;
-
+        private MainContext context;
         public MainWindow()
         {
             InitializeComponent();
-            _shape = new ShapeData();
-            _shapes = new List<ShapeData>();
-            SetBinding(TitleProperty, new Binding("FileInfo.Name") { Source = this, StringFormat = "VPaint - {0}" });
-            ToolBar.DataContext = _shape;
+            context = Resources["MainContext"] as MainContext;
         }
 
-        public FileInfo FileInfo
+
+
+        private void SetBrushCommandBinding_OnExecuted(object sender, ExecutedRoutedEventArgs e)
         {
-            get { return (FileInfo)GetValue(FileInfoProperty); }
-            set { SetValue(FileInfoProperty, value); }
-        }
-
-        // Using a DependencyProperty as the backing store for FileInfo.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty FileInfoProperty =
-            DependencyProperty.Register("FileInfo", typeof(FileInfo), typeof(MainWindow), new UIPropertyMetadata(new FileInfo("Безымянный.vif")));
-
-
-
-        private void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-            if (e.ChangedButton == MouseButton.Left)
-            {
-                Point position = e.GetPosition(DrawCanvas);
-                ShapeData shape = _shape.Clone();
-                shape.X = position.X;
-                shape.Y = position.Y;
-                _shapes.Add(shape);
-                Polygon polygon = MakePolygon(shape);
-                DrawCanvas.Children.Add(polygon);
-            }
-        }
-
-        private Polygon MakePolygon(ShapeData shape)
-        {
-            Polygon polygon = new Polygon
-            {
-                Fill = shape.Background,
-                Stroke = shape.Border,
-                StrokeThickness = shape.BorderWidth,
-                Points = new PointCollection
-                {
-                    new Point(0, 10),
-                    new Point(0, 30),
-                    new Point(100, 30),
-                    new Point(100, 40),
-                    new Point(120, 20),
-                    new Point(100, 0),
-                    new Point(100, 10)
-                }
-            };
-            polygon.SetValue(Canvas.LeftProperty, shape.X);
-            polygon.SetValue(Canvas.TopProperty, shape.Y);
-            return polygon;
-        }
-
-        private void CommandBinding_OnExecuted(object sender, ExecutedRoutedEventArgs e)
-        {
-            BrushDialog bd = new BrushDialog { ResultData = _shape.Clone() };
+            BrushDialog bd = new BrushDialog { ResultData = context.ShapeData.Clone() };
             if (bd.ShowDialog() == true)
-                ToolBar.DataContext = _shape = bd.ResultData.Clone();
+                context.ShapeData = bd.ResultData.Clone();
         }
 
         private void SaveAsCommandBinding_OnExecuted(object sender, ExecutedRoutedEventArgs e)
@@ -93,14 +40,14 @@ namespace Lab5
             FileDialog fd = GetFileDialog<SaveFileDialog>();
             if (fd.ShowDialog() == true)
             {
-                File.WriteAllLines((FileInfo = new FileInfo(fd.FileName)).FullName, _shapes.ConvertAll(input => input.ToString()));
+                context.Save(fd.FileName);
                 MessageBox.Show("Успех");
             }
         }
 
         private void SaveCommandBinding_OnCanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
-            e.CanExecute = _shapes.Count > 0;
+            e.CanExecute = context.CanSave;
         }
 
         private void OpenCommandBinding_OnExecuted(object sender, ExecutedRoutedEventArgs e)
@@ -108,32 +55,39 @@ namespace Lab5
             FileDialog fd = GetFileDialog<OpenFileDialog>();
             if (fd.ShowDialog() == true)
             {
-                _shapes.Clear();
-                DrawCanvas.Children.Clear();
-                IEnumerable<string> lines = File.ReadLines((FileInfo = new FileInfo(fd.FileName)).FullName);
-                foreach (string line in lines)
-                {
-                    ShapeData shapeData = (ShapeData)line;
-                    _shapes.Add(shapeData);
-                    Polygon polygon = MakePolygon(shapeData);
-                    DrawCanvas.Children.Add(polygon);
-                }
+                context.Load(fd.FileName);
             }
         }
 
-        private void SaveCommandBinding_OnExecuted(object sender, ExecutedRoutedEventArgs e)
-        {
-            File.WriteAllLines(FileInfo.FullName, _shapes.ConvertAll(input => input.ToString()));
-        }
-
-        private FileDialog GetFileDialog<T>() where T : FileDialog, new()
-        {
-            return new T { InitialDirectory = FileInfo.DirectoryName, FileName = FileInfo.Name, Filter = "Файлы Vector Image Format (vif)|*.vif|Все файлы|*.*" };
-        }
 
         private void AboutCommandBinding_OnExecuted(object sender, ExecutedRoutedEventArgs e)
         {
             new About().ShowDialog();
+        }
+
+        /**********************************************************************/
+        #region Canvas events
+        /**********************************************************************/
+        private void DrawCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point position = e.GetPosition(sender as IInputElement);
+            context.ShapeData.Position = position;
+        }
+
+        private void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                context.AddShape();
+            }
+        }
+
+        #endregion
+
+
+        private FileDialog GetFileDialog<T>() where T : FileDialog, new()
+        {
+            return new T { InitialDirectory = context.FileInfo.DirectoryName, FileName = context.FileInfo.Name, Filter = "Файлы Vector Image Format (vif)|*.vif|Все файлы|*.*" };
         }
     }
 }
